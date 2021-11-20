5b9d4fe1b8e115d1353eb75cf2e8b875c482dbc5
Otoniel Oliveira <otonielloliveira@gmail.com>
2021-11-20 12:26:59 -0300

    correcao api

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken, logout } from "./Auth";
 const baseURL = "http://api77.test/"
-const baseURLProd = ""
+const baseURLProd = "https://api77.yellowsistemas.com.br/"
 const api = axios.create({
   baseURL: (process.env.NODE_ENV === 'production' ? baseURLProd : baseURL)
 });
